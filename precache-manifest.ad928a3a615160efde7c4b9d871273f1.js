b5de4ee5d0b6325a8e089147ed857241d1b681c6
ashish7777777 <ashsih100rana123@gmail.com>
2020-10-14 15:40:03 +0530

    Updates

---
+++ precache-manifest.ad928a3a615160efde7c4b9d871273f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a530bcb4917d71b1061b44c8e97350",
+    "url": "/LastTest/index.html"
+  },
+  {
+    "revision": "a65d0011e728c2f16a66",
+    "url": "/LastTest/static/css/main.09ca6ee7.chunk.css"
+  },
+  {
+    "revision": "eba8274f4a988bad8f26",
+    "url": "/LastTest/static/js/2.a579dd73.chunk.js"
+  },
+  {
+    "revision": "93ce851949c81c033c28f6758a3c33f3",
+    "url": "/LastTest/static/js/2.a579dd73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a65d0011e728c2f16a66",
+    "url": "/LastTest/static/js/main.48e8c081.chunk.js"
+  },
+  {
+    "revision": "7e3b8c272660baec8568",
+    "url": "/LastTest/static/js/runtime-main.70c044bc.js"
+  }
+]);
